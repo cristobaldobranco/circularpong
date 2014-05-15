7c0ab6494e665b942df36ee604b4f79cfa9f4f07
Christoph Weiß <christoph.weiss@gmail.com>
2014-05-15 15:50:37 +0200

    Initial commit

---
+++ desktop/src/org/whitesoft/circularpong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.whitesoft.circularpong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.whitesoft.circularpong.CircularPongMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CircularPongMain(), config);
+	}
+}
---
+++ html/src/org/whitesoft/circularpong/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package org.whitesoft.circularpong.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import org.whitesoft.circularpong.CircularPongMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new CircularPongMain();
+        }
+}
